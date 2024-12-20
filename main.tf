973b9157725683e23a115043db69d6f7a656c2d4
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-20 12:10:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,115 @@
+provider "aws" {
+  region = "us-west-2"  # You can specify the AWS region here
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_a"
+  }
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_b"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route" "internet_route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id            = aws_internet_gateway.igw.id
+}
+
+resource "aws_security_group" "allow_http" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_security_group" "allow_https" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+
+  tags = {
+    Name = "allow_https"
+  }
+}
+resource "aws_instance" "web_server" {
+  ami           = "ami-05d38da78ce859165"  # Use the appropriate AMI ID for your region (Amazon Linux 2)
+  instance_type = "t2.micro"               # Use the appropriate instance type
+#  key_name      = "new-key"           # Replace with your SSH key name
+
+  # User data script to install Apache web server
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<html><h1>Welcome to my Web Server!</h1></html>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+
+# Outputs the public IP address of the EC2 instance
+output "instance_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
